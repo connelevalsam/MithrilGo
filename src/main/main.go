c62d0f857e862bda2f798a0c0d6a9cce5ba1dafe
connelblaze <connelblaze@gmail.com>
2019-02-27 18:51:57 +0100

    initial commit

---
+++ src/main/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"fmt"
+)
+
+var (
+	err error
+	templ *template.Template
+)
+
+func init() {
+	templ, err = templ.ParseGlob("templates/*.html")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+	http.Handle("/assets/", http.FileServer(http.Dir(".")))
+	fmt.Println("server running on port :9090")
+	http.ListenAndServe(":9090", nil)
+}
+func index(writer http.ResponseWriter, request *http.Request) {
+	title := "Home"
+	err = templ.ExecuteTemplate(writer, "index.gohtml", title)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(writer, "there was an error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Name of page: "+title)
+}
+
+func about(writer http.ResponseWriter, request *http.Request) {
+	title := "About"
+	/*
+	err = templ.ExecuteTemplate(writer, "about.html", title)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(writer, "there was an error", http.StatusInternalServerError)
+		return
+	}*/
+	fmt.Println("Name of page: "+title)
+}
+
+func contact(writer http.ResponseWriter, request *http.Request) {
+	title := "Contact"
+	/*
+	err = templ.ExecuteTemplate(writer, "contact.html", title)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(writer, "there was an error", http.StatusInternalServerError)
+		return
+	}*/
+	fmt.Println("Name of page: "+title)
+}
